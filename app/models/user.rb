f3bc1c03b6cd6b48a987b6e81e813c95c14ae2d7
Donald Brooks <brooksd079@gmail.com>
2022-10-10 21:47:47 +0300

    user models validations added for input fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :expenses
     has_many :categories, through: :expenses
+
+    #validations for the user model
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :username, presence: true, uniqueness: true
 end
