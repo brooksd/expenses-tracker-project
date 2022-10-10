c25f2d5bb59c5054fb202f203b3e3949b072967c
Donald Brooks <brooksd079@gmail.com>
2022-10-10 21:22:55 +0300

    migrations poulated and schema created

--- db/migrate/20221010181340_create_expenses.rb
+++ db/migrate/20221010181340_create_expenses.rb
@@ -1,6 +1,9 @@
 class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
+      t.decimal :amount
+      t.integer :user_id
+      t.integer :category_id
 
       t.timestamps
     end
