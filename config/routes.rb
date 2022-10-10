97f32c74c01ad2aad66345a9a92c8b38e4ff80ff
Donald Brooks <brooksd079@gmail.com>
2022-10-10 22:35:35 +0300

    controllers generated for all three models and appropriate routes created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #main controllers 
+  resources :users, only: [:index, :create, :update ] do
+    resources :expenses, only [:index, :create, :show, :update, :destroy]
+  end
+  resources :categories, only[:index]
 end
