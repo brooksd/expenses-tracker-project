3563b0fd276d1a599d55ee1d3c1d762bc9b76eae
Donald Brooks <brooksd079@gmail.com>
2022-10-10 21:44:35 +0300

    model relationships and associations added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_181340) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_10_182450) do
   create_table "categories", force: :cascade do |t|
     t.string "category"
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_181340) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
   end
 
   create_table "users", force: :cascade do |t|
