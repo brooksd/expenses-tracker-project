1bf8c2bb685b08805c1b62aec8aef5dc1adf31ba
Donald Brooks <brooksd079@gmail.com>
2022-10-11 05:55:24 +0300

    adds destroy action to the expenses controller and validation security to limit who can view expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,8 @@
 class ExpensesController < ApplicationController
+    before_action :user_authorized_to_see
 
     def index
-        user = User.find(params[:user_id])
+        user = user_find_params
         expenses = user.expenses
         render json: expenses, status: :ok
     end
@@ -12,7 +13,7 @@ class ExpensesController < ApplicationController
     end
 
     def create
-        user = User.find(params[:user_id])
+        user = user_find_params
         expenses = user.expenses.create!(expense_params)
         render json: expenses, status: :created
     end
@@ -23,13 +24,31 @@ class ExpensesController < ApplicationController
         render json: expense
     end
 
+    def destroy
+        user = user_find_params
+        expense = user.expenses
+        expense = expense_find_params
+        expense.destroy
+        head :no_content
+    end
+
     private
 
     def expense_find_params
         Expense.find(params[:id])
     end
 
+    def user_find_params
+        User.find(params[:user_id])
+    end
+
     def expense_params
         params.require(:expense).permit(:id, :amount, :user_id, :category_id, :date)
+    end
+
+    def user_authorized_to_see
+        user = user_find_params
+        user_permitted = @current_user.id == user.id
+        render json: { error: "You are not Authorized to view other's expenses" }, status: :forbidden unless user_permitted
     end
 end
