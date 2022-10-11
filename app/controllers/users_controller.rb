c6066938facd324b194b743dca500d3032c9ccf3
Donald Brooks <brooksd079@gmail.com>
2022-10-11 05:03:19 +0300

    adds create and show actions to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create!(user_params)
+        session[user_id] = user.id 
+        render json: user, status: :created
+    end
     
+    def show
+        redner json: @current_user
+    end
+
+    private 
+
+    def user_params
+        params.permit(:first_name, :last_name, :username, :password, :password_confirmation)
+    end 
 end
