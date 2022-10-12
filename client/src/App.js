c736418d16f551f2941b35e0648d6b9162b7b252
Donald Brooks Arum <brooksd079@gmail.com>
2022-10-12 20:03:34 +0300

    login form added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import SignupForm from './components/SignupForm';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
      Hello From React, Brooks 
      <SignupForm />
+     <LoginForm />
     </div>
   );
 }
