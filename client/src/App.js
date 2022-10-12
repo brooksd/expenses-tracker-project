3fb63181872b6b4f86a2fa738731071f5a70a015
Donald Brooks <brooksd079@gmail.com>
2022-10-12 06:05:51 +0300

    SignUp form added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SignupForm from './components/SignupForm';
 
 function App() {
   return (
     <div className="App">
      Hello From React, Brooks 
+     <SignupForm />
     </div>
   );
 }
