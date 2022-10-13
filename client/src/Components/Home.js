8b9d7f063247900e427d6d55315d0fae86db94b2
Donald Brooks <brooksd079@gmail.com>
2022-10-14 00:11:20 +0300

    home page styling progress

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -99,7 +99,7 @@ const Home = ({setUser, user}) => {
 
 
     return (
-      <Container className="m-0 p-0 container-fluid">
+      <Container className="m-0 p-0 container">
         <div className="row d-flex h-100 ">
           <div className="col-3 bg-primary border border-danger m-0">
             <div class="wrapper">
@@ -112,34 +112,12 @@ const Home = ({setUser, user}) => {
                 <p>Dummy Heading</p>
                 <li class="active">
                     <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
-                    <ul class="collapse list-unstyled" id="homeSubmenu">
-                        <li>
-                            <a href="#">Home 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 3</a>
-                        </li>
-                    </ul>
                 </li>
                 <li>
                     <a href="#">About</a>
                 </li>
                 <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
+                    <a href="#pageSubmenu">Pages</a>
                 </li>
                 <li>
                     <a href="#">Portfolio</a>
