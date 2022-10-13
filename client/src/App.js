c93e97272fbc86b4469074e1bbf9185f2fdac6a3
Donald Brooks <brooksd079@gmail.com>
2022-10-13 10:16:03 +0300

    app component fifnished with relevant imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,53 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 function App() {
+  
+  const [user, setUser] = useState(null);
+  let history = useHistory();
+  
+  useEffect(() => {
+  fetch("/me").then((res) => {
+   if (res.ok) {
+    res.json().then((user) => setUser(user));
+    }
+   });
+  }, []);
+
+
+  function onLogin(loguser){
+    setUser(loguser)
+    history.push("/home")
+  }
+
+  if (!user){
+    return <div>
+      <Switch>
+        <Route exact path="/">
+          <LoginForm onLogin={onLogin} user={user}/>
+        </Route>  
+        <Route exact path="/sign">
+          <SignupForm onLogin={onLogin}/>
+        </Route>
+      </Switch>
+    </div>
+  }
+
   return (
     <div className="App">
-     Hello From React, Brooks 
-     <SignupForm />
-     <LoginForm />
+      Hello From React, Brooks
+      <NavBar user={user} />
+      <Switch>
+        <Route
+          exact
+          path="/home"
+          component={() => <Home setUser={setUser} user={user} />}
+        />
+      </Switch>
     </div>
   );
 }
